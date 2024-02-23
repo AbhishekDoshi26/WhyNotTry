051c5470eb17365b1a6cfbf0d203cf87cedc1bee
Abhishek Doshi <adoshi26.ad@gmail.com>
2024-02-23 17:30:10 +0530

    Initial Commit

---
+++ WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by Abhishek Doshi on 23/02/24.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
