090f01f6da9f3204fd73aec310a51ffbd0555adc
Abhishek Doshi <adoshi26.ad@gmail.com>
2024-02-23 18:06:47 +0530

    first try for swift ui app

--- WhyNotTry/ContentView.swift
+++ WhyNotTry/ContentView.swift
@@ -1,21 +1,39 @@
-//
-//  ContentView.swift
-//  WhyNotTry
-//
-//  Created by Abhishek Doshi on 23/02/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    var activities = ["Archery", "Baseball", "Basketball", "Bowling", "Boxing", "Cricket", "Curling", "Fencing", "Golf", "Hiking", "Lacrosse", "Rugby", "Squash"]
+    
+    @State private var selected = "Archery"
+    
+    var colors: [Color] = [.blue, .cyan, .gray, .green, .indigo, .mint, .orange, .pink, .purple, .red]
+    @State private var key = 0;
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("Why not try…")
+               .font(.largeTitle.bold())
+        Spacer()
+        VStack(content: {
+            Circle()
+                .fill(
+                    colors.randomElement() ?? .blue
+                )
+                .padding()
+                .overlay(
+                    Image(
+                        systemName: "figure.\(selected.lowercased())"
+                    ).font(.system(size: 100)).foregroundColor(.white)
+                )
+            Text("\(selected)!").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+        }).transition(.slide).id(key)
+        
+        Spacer()
+        Button("Change Action!", action: {
+            withAnimation(.easeInOut(duration: 0.5)) {
+                selected = activities.randomElement() ?? "Archery"
+                key+=1
+            }
+            
+        }).padding().buttonStyle(.bordered)
     }
 }
 
